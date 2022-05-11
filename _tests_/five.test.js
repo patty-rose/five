dab8cb07c0d7371c0ca1bde56c4e1a7cccee1fd1
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-05-10 19:22:08 -0700

    pass test for ordering inputted numbers

--- _tests_/five.test.js
+++ _tests_/five.test.js
@@ -1,7 +1,9 @@
 import createArray from './../src/five.js'
 
 describe('createArray', () => {
-  test('should return one numbers', () => {
-    expect(createArray(2)).toBe(2);
-  })
+  test('should return an array with user inputted numbers, lowest number at 0 index and highest number at the final index place', () => {
+    const answer = createArray(28, 2);
+    expect(answer.slice(0, 1)).toEqual([2]);
+    expect(answer.slice(-1)).toEqual([28]);
+  });
 });
--- src/five.js
+++ src/five.js
@@ -1,5 +1,10 @@
 const { TestScheduler } = require("jest");
 
-export default function createArray(num1){
-  return (num1);
+export default function createArray(num1, num2){
+  let numArray = [];
+  const startNumber = num1;
+  const  endNumber = num2;
+  numArray = [startNumber, endNumber];
+
+  return numArray.sort();
 }
